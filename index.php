ee6d97129d57a7cdbbaa694010b12761aa57c9c8
SK. Nur Alam <100152180+noor4964@users.noreply.github.com>
2025-07-29 21:21:09 +0600

    index modified

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <Title>
-            Home | Online Classroom Managemnet
-        </Title>
-        <link rel ="stylesheet" href="assets/css/style.css">
-    </head>
-    <body>
-        <div class="container">
-            <header>
-                <h1>Online Classroom Management System</h1>
-            </header>
-        </div>
-        <main>
-            <div class = "welcome-section" >
-                <h2>Welcome to the Online Classroom</h2>
-                <p>Manage your classes, assignments, and students all in one place.</p>
-                <div class = action-buttons>
-                    <a href="login.php" class="btn btn-login">Login</a>
-                    <a href="register.php" class="btn btn-register">Register</a>
-    
-                </div>
-            </div>
+
+<head>
+    <Title>
+        Home | Online Classroom Managemnet
+    </Title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Online Classroom Management System</h1>
+        </header>
 
         <main>
-    </body>
+            <div class="welcome-section">
+                <h2>Welcome to the Online Classroom</h2>
+                <p>Manage your classes, assignments, and students all in one place.</p>
+                <div class=action-buttons>
+                    <a href="login.php" class="btn btn-login">Login</a>
+                    <a href="register.php" class="btn btn-register">Register</a>
+
+                </div>
+            </div>
+    </div>
+
+    <main>
+</body>
+
 </html>
